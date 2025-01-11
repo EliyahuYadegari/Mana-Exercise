3937c00de1568a7964212603dbfe4878a3a3ef10
Eli <yadgare10@gmail.com>
2025-01-11 22:52:25 +0200

    fix store to database bug

--- database.py
+++ database.py
@@ -20,23 +20,45 @@ class Database:
         conn.commit()
         conn.close()
 
-
     def store_results(self, results_df):
         if results_df.empty:
             st.warning("No data to store.")
             return
 
-        conn = sqlite3.connect(self.db_name)
-        
-        st.write("---עד כאן מופיע---")
+        # Convert unsupported data types to string or a supported type
+        for col in results_df.columns:
+            if results_df[col].dtype == 'object' or pd.api.types.is_datetime64_any_dtype(results_df[col].dtype):
+                results_df[col] = results_df[col].astype(str)
 
+        conn = sqlite3.connect(self.db_name)
+
+        st.write("---Before storing data---")
         st.dataframe(results_df)
 
-        results_df.to_sql('data_table', conn, if_exists='append', index=False)
+        try:
+            results_df.to_sql('data_table', conn, if_exists='append', index=False)
+            st.write("---Data successfully stored---")
+        except Exception as e:
+            st.error(f"An error occurred during SQL operation: {e}")
+        finally:
+            conn.close()
 
-        st.write("---זה כבר לא מופיע---")
+    # def store_results(self, results_df):
+    #     if results_df.empty:
+    #         st.warning("No data to store.")
+    #         return
 
-        conn.close()
+    #     conn = sqlite3.connect(self.db_name)
+        
+    #     st.write("---עד כאן מופיע---")
+
+    #     st.dataframe(results_df)
+
+    #     results_df.to_sql('data_table', conn, if_exists='append', index=False)
+
+    #     st.write("---זה כבר לא מופיע---")
+
+    #     conn.close()
 
 
 
